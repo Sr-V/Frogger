3c359a0f04bb2bbcda9ed5045f593dab13525864
Sr-V <victor.d.f.11702@gmail.com>
2025-02-21 12:39:07 +0100

    - Añadida una función de cierre de sesión usando `FirebaseAuthManager`. - Implementada la lógica para redirigir a `LoginActivity` después de cerrar sesión. - Añadido el botón `btnGoogleLogout`.

--- app/src/main/java/edu/pmdm/frogger/activities/MainActivity.java
+++ app/src/main/java/edu/pmdm/frogger/activities/MainActivity.java
@@ -1,6 +1,8 @@
-package edu.pmdm.frogger;
+package edu.pmdm.frogger.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,40 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import edu.pmdm.frogger.R;
+import edu.pmdm.frogger.firebase.FirebaseAuthManager;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "FroggerMain";
+    private FirebaseAuthManager authManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        authManager = FirebaseAuthManager.getInstance(this);
+
+        findViewById(R.id.btnGoogleLogout).setOnClickListener(v -> signOut());
+    }
+
+    private void signOut() {
+        authManager.signOut(this, task -> {
+            if (task.isSuccessful()) {
+                Log.d(TAG, "Google sign out successful");
+            }
+            // Redirige a LoginActivity
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
         });
     }
 }
