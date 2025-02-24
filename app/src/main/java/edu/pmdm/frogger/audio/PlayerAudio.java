94e4134397a02d1dd6316967171d00c235b6951a
Alejandro <alejandrosanchezgil7@gmail.com>
2025-02-25 00:29:56 +0100

    Implementados más métodos de audio para el jugador.

--- app/src/main/java/edu/pmdm/frogger/audio/PlayerAudio.java
+++ app/src/main/java/edu/pmdm/frogger/audio/PlayerAudio.java
@@ -2,6 +2,8 @@ package edu.pmdm.frogger.audio;
 
 import android.content.Context;
 import android.media.MediaPlayer;
+import android.os.Handler;
+
 
 import edu.pmdm.frogger.R;
 
@@ -18,6 +20,14 @@ public class PlayerAudio {
             }
         });
 
+        MediaPlayer mp2 = MediaPlayer.create(c, R.raw.frog_ribbit);
+        mp2.start();
+        mp2.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                mp2.release();
+            }
+        });
 
     }
 
@@ -31,6 +41,36 @@ public class PlayerAudio {
                 mp.release();
             }
         });
+    }
+
+    public void wallCollision(Context c){
+        MediaPlayer mp = MediaPlayer.create(c, R.raw.frog_wall_collision);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                mp.release();
+            }
+        });
+    }
+
+    public void idleCroak(Context c){
+        MediaPlayer mp = MediaPlayer.create(c, R.raw.frog_croak);
+        try {
+            mp.wait(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        mp.start();
+        mp.setLooping(true);
+        new Handler().postDelayed(mp::start, 5000);
+
+        new Handler().postDelayed(() ->{
+            if(mp.isPlaying()){
+                mp.stop();
+                mp.release();
+            }
+        }, 20000);
     }
 
 }
